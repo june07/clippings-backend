9cca56dbf32a7ad25233a48c5e6b27cbdfa99551
Will <will@onezerohosting.com>
2026-08-20 11:18:10 -0500

    .

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,9 +2,16 @@ const { createClient } = require('redis')
 
 const logger = require('./logger')
 const config = require('./config')
+
+const { REDIS_URL, REDIS_HOST_PASSWORD, REDIS_PORT, REDIS_DB } = config
+const redisURL = REDIS_URL || `redis://default:${REDIS_HOST_PASSWORD}@redis:${REDIS_PORT || 6379}/${parseInt(REDIS_DB, 10) || 0}` || 'redis://redis'
+const redisURLSafe = REDIS_URL || `redis://default:${REDIS_HOST_PASSWORD.replace(/.*/g, '****')}@redis:${REDIS_PORT || 6379}/${parseInt(REDIS_DB, 10) || 0}` || 'redis://redis'
+
+console.log('Redis URL:', redisURLSafe)
 const redis = createClient({
-    url: config.NODE_ENV === 'production' ? `redis://default:${config.REDIS_HOST_PASSWORD}@redis` : 'redis://redis',
+    url: redisURL,
 })
+
 redis.on('error', err => logger.error('Redis Client Error', err))
 redis.connect().then(redis => {
     logger.info('Connected to Redis')
